d229ee46de24e4f6f8ef86984590769541635fe8
Yuriy Eliseev <yurij.eliseev.2004@mail.ru>
2023-11-27 19:43:23 +0700

    Добавил логику авторизации и генерации ее карточки

---
+++ scripts/validationForm.js
@@ -0,0 +1,18 @@
+export function Validate(){
+    $("form").validate({
+        rules: {
+            email: {
+                required: true,
+                email: true
+            },
+            loginPassword: "required"
+        },
+        messages: {
+            email: {
+                required: "Введите email",
+                email: "Введите корректный email"
+            },
+            loginPassword: "Введите пароль"
+        }
+    })
+}
